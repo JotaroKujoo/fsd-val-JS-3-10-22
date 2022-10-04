7dedd385bc2d9eea8c678904638f1612a820decd
Jose Rodriguez <rdrgz.jose123@gmail.com>
2022-10-04 15:26:41 +0200

    Actualizando trabajo al ejercitado en clase

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// let edad = 21;
+
+// let resultado = (edad <= 16) ? "No puedes conducir en usa" : "Si";
+
+
+// let dinero = 50;
+
+// while(dinero >=0){
+//     console.log("Estoy comprando y me quedan", dinero, " euros")
+//     dinero--;
+// }
+
+// let contador = 0;
+// let num = 3;
+
+// while (contador<=10){
+//     console.log(num,"multiplicado por",contador,"es igual a ",num*contador)
+//     contador+=1
+
+// };
+
+
+// let numero = 36;
+
+// do {
+//     console.log("David es un genio")
+// } while (numero < 36);
+
+// BUCLE FOR
+
+// let tabla = 3;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(tabla*i)
+
+// }
+
+// FUNCIONES
+
+// function Sumar(num1, num2){
+//     let resultado = num1 + num2;
+//     return resultado
+// }
+
+// function Dividir(num1, num2){
+//     let resultado =num1 / num2;
+//     return resultado
+// }
+
+
+// let resultadoDeSuma = Sumar(3,4);
+// console.log(resultadoDeSuma)
+
+// let resultadoDeDividir = Dividir(8,2)
+// console.log(resultadoDeDividir)
+
+// function Elevar(num1, num2){
+//     let resultado = num1 ** num2;
+//     return resultado
+// };
+
+// let operador = parseInt(prompt("Introduce el numero a elevar..."));
+// let base = parseInt(prompt("Introduce a cuanto lo quieres elevar..."));
+
+// let resultadoElevar = Elevar(operador, base);
+// console.log(resultadoElevar);
+
+// let manelArrayHobies = [
+//     "Tocar el bajo",
+//     "Leer",
+//     "Cine",
+//     "Programación"
+// ]
+
+let numeros = [];
+
+for(let i = 1; i<=100; i++){
+    numeros.push(i)
+}
